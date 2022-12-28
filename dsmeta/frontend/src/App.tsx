94fab08a21c99904f96c456629b436553ef4b570
Eron Moraes <eron.moraes7@gmail.com>
2022-12-28 11:02:27 -0300

    first component

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá Mundo!</h1>
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
