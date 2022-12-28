52d8e822aee92a467ca336c6b8847140104df665
Eron Moraes <eron.moraes7@gmail.com>
2022-12-28 13:58:57 -0300

    styles

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+              <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
-
+ 
 export default App
