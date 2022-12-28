3671d97ae019dab06969f3e7da5f28a2ddf2631c
Eron Moraes <eron.moraes7@gmail.com>
2022-12-28 14:13:17 -0300

    sales card

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import NotificationButton from "./components/NotificationButton"
+
 import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
