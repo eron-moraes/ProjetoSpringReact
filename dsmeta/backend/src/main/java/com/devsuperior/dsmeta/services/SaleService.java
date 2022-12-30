7b520bf1e1cf2bff5692b95957a36ea530d4ea38
Eron Moraes <eron.moraes7@gmail.com>
2022-12-30 10:25:54 -0300

    value min and max date

--- dsmeta/backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ dsmeta/backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,8 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -14,8 +14,8 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(Pageable pageable) {
+		return repository.findAll(pageable);
 	
 	}
 
