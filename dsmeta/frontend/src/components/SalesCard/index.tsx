eb42575ab75c4d555548e39b3fc090138c5ef645
Eron Moraes <eron.moraes7@gmail.com>
2023-01-02 21:12:32 -0300

    send notification

--- dsmeta/frontend/src/components/SalesCard/index.tsx
+++ dsmeta/frontend/src/components/SalesCard/index.tsx
@@ -22,8 +22,6 @@ function SalesCard() {
         const dmin = minDate.toISOString().slice(0,10);
         const dmax = maxDate.toISOString().slice(0,10);
 
-        console.log(dmin);
-
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
